995fe1faa9107a75e75f6c75574cf54d2488e9cd
JuliaZajusz <julia.zajusz@poczta.fm>
2017-07-06 12:41:06 +0200

    footer

--- footer.php
+++ footer.php
@@ -14,10 +14,42 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<div class="container">ASDASDASDASDASDASDASDASD</div>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<div class="container">
+			<div class="newsletter">
+			
+			</div>
+
+			<div class="site-info row">
+				<!--<?php  //komentarz//wp_nav_menu(array('theme_location'=>'secondary')); ?> -->
+
+
+				<div class="col-xs-6 col-md-4 regulaminy">
+					<a href="">Pobierz regulaminy SKIACTIVE</a>
+				</div>
+
+
+				<div class="col-xs-6 col-md-4 icons" >
+					<img src="<?php echo get_template_directory_uri(); ?>/img/face-icon.png" alt="facebook icon" class="img-responsive img-fluid">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/insta-icon.png" alt="instagram icon" class="img-responsive img-fluid">
+				</div>
+
+
+				<div class="col-xs-6 col-md-4 policy">
+					<div class="policy-links">
+						<a href="">Polityka cookies</a>
+						<p>|</p>
+						<a href="">Polityka prywatności</a>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+
+		<div class="footerimage">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/footer-mountains.png" alt="mountains">
+		</div>
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
