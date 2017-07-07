e913f5c43694607ecfa02bd0509f0223743e9fc1
JuliaZajusz <julia.zajusz@poczta.fm>
2017-07-07 15:29:18 +0200

    aboutUs  site without click events

---
+++ aboutUs.php
@@ -0,0 +1,237 @@
+<?php
+/**
+* The template for displaying all pages
+*
+* This is the template that displays all pages by default.
+* Please note that this is the WordPress construct of pages
+* and that other 'pages' on your WordPress site may use a
+* different template.
+*
+*Template Name: AboutUs
+*
+* @link https://codex.wordpress.org/Template_Hierarchy
+*
+* @package SkiActive
+*/
+get_header(); ?>
+<div class="aboutUs">
+	<div class="container">
+		
+		<div class="we row">
+			<div class="text col-xs-6 col-md-6">
+				<h1>O NAS</h1>
+				<div class="underline"></div>
+				<p class="textlead">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, delectus! Obcaecati alias eaque dolorem est eveniet repudiandae nesciunt amet.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci itaque unde cum sequi fugit ullam incidunt velit, atque dignissimos modi laborum iure ut repellat voluptates nobis totam animi odio, placeat facilis nulla sunt ea, laboriosam culpa officia dolorum. 
+					<br><br>
+					Deserunt ab expedita unde laborum, minus sequi consequuntur quis sint doloremque culpa velit nihil voluptates, ipsa incidunt alias accusamus molestiae ipsum atque rerum magnam dolorem magni, autem ad odit! Illum quisquam delectus non explicabo repellendus voluptatibus laboriosam, obcaecati, consequuntur rem eos porro, suscipit, necessitatibus optio nostrum amet hic. Saepe obcaecati, ex natus nulla facilis soluta quae, voluptas magni a non! Voluptates, sapiente. Laboriosam culpa officia dolorum. 
+					<br><br>
+					Deserunt ab expedita unde laborum, minus sequi consequuntur quis sint doloremque culpa velit nihil voluptates, ipsa incidunt alias accusamus molestiae ipsum atque rerum magnam dolorem magni, autem ad odit! Illum quisquam delectus non explicabo repellendus voluptatibus laboriosam, obcaecati, consequuntur rem eos porro, suscipit, necessitatibus optio nostrum amet hic. Saepe obcaecati, ex natus nulla facilis soluta quae, voluptas magni a non! Voluptates, sapiente.
+				</p>		
+			</div>
+			
+			<div class="photos col-xs-6 col-md-6">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/materialydostrony_046.jpg" alt="placeholder" class="img-responsive img-fluid ">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/materialydostrony_089.jpg" alt="placeholder" class="img-responsive img-fluid ">
+			</div>
+
+		</div>
+		
+
+
+
+		<div class="infographic">
+			
+		</div>
+
+
+
+
+		<div class="ourteam">
+			<h1>NASZ ZESPÓŁ</h1>
+			<div class="underline"></div>
+
+			<div class="section1 col-xs-8 col-md-8 col-xs-offset-2 row">
+
+				<div class="owner-box col-xs-6 col-md-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Łukasz Kuźmiński</h4>
+					<p>co-owner</p>
+				</div>
+				
+				<div class="owner-box col-xs-6 col-md-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Rafał Sionek</h4>
+					<p>co-owner</p>
+				</div>
+
+			</div>
+
+			<div class="section2 col-xs-8 col-md-8 col-xs-offset-2">
+
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+				
+				<div class="employee-box col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+					<h4>Jan Kowalski</h4>
+					<p>stanowisko</p>
+				</div>
+			
+
+			</div>
+			
+		</div>
+
+
+		
+		
+
+
+
+
+
+		<div class="directions ">
+			
+			
+			<div class="section1 row">
+				<h1>KIERUNKI WYJAZDÓW</h1>
+				<div class="underline"></div>
+				<div class="col-xs-12 col-md-12 ">
+					<p class="col-xs-8 col-md-8 col-xs-offset-2 col-md-offset-2">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque error nostrum voluptatum quos maiores, tenetur atque laboriosam sint quae facere aspernatur corrupti porro soluta eos. Maiores ut deserunt, fuga impedit. Consectetur adipisicing elit. Odio asperiores nemo, quod quae quis esse eveniet mollitia dicta dolor sit voluptate, in praesentium iure, voluptatem ratione harum commodi necessitatibus nostrum!...
+					</p>
+				</div>
+			</div>
+
+			<div class="gallery-slider row">
+			</div>
+
+		</div>
+
+
+
+
+		<div class="licences">
+			<h1>LICENCJE</h1>
+			<div class="underline"></div>
+			<div class="col-xs-12 col-md-12 box">
+				<p class="col-xs-8 col-md-8 col-xs-offset-2 col-md-offset-2">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque error nostrum voluptatum quos maiores, tenetur atque laboriosam sint quae facere aspernatur corrupti porro soluta eos. Maiores ut deserunt, fuga impedit. Consectetur adipisicing elit. Odio asperiores nemo, quod quae quis esse eveniet mollitia dicta dolor sit voluptate, in praesentium iure, voluptatem ratione harum commodi necessitatibus nostrum!...
+				</p>
+			</div>
+			
+			<div class="box">
+				<div class="partner-logos row">
+					<div class="partner-logo col-xs-4 col-md-4">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo_sitn.jpg" alt="logo SITN PZN" class="img-responsive img-fluid">
+					</div>
+					<div class="partner-logo col-xs-4 col-md-4">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo_fischer.jpg" alt="logo Fischer" class="img-responsive img-fluid">
+					</div>
+					<div class="partner-logo col-xs-4 col-md-4">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo_brikomaplus.jpg" alt="logo BRIKO-MAPLUS" class="img-responsive img-fluid">
+					</div>
+				</div>
+				<div class="partner-description row">
+					<div class="partner-desc col-xs-4 col-md-4">
+						<p>
+							Licencja SITN PZN<br>od 2004 r.
+						</p>
+					</div>
+					<div class="partner-desc col-xs-4 col-md-4">
+						<p>
+							Licencja<br>FISCHER
+						</p>
+					</div>
+					<div class="partner-desc col-xs-4 col-md-4">
+						<p>
+							Autoryzacja<br>BRIKO-MAPLUS
+						</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+
+
+
+
+
+		<div class="partners">
+			<h1>PARTNERZY</h1>
+			<div class="underline"></div>
+			<div class="partner-logos row">
+				<div class="partner-logo col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo_cam.jpg" alt="logo CAMdivision" class="img-responsive img-fluid">
+				</div>
+				<div class="partner-logo col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo_briko.jpg" alt="logo Briko" class="img-responsive img-fluid">
+				</div>
+				<div class="partner-logo col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo_fischer.jpg" alt="logo Fischer" class="img-responsive img-fluid">
+				</div>
+				<div class="partner-logo col-xs-3 col-md-3">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo_europa.jpg" alt="logo Europa" class="img-responsive img-fluid">
+				</div>
+			</div>
+			
+		</div>
+
+	</div>
+</div>
+<?php get_footer(); ?>
