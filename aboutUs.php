e4e3909681a2971d7a4b78764050d8b6c9c956b4
JuliaZajusz <julia.zajusz@poczta.fm>
2017-07-07 17:07:16 +0200

    pop up owner

--- aboutUs.php
+++ aboutUs.php
@@ -15,6 +15,7 @@
 */
 get_header(); ?>
 <div class="aboutUs">
+<div class="overlay hidden"></div>
 	<div class="container">
 		
 		<div class="we row">
@@ -56,7 +57,7 @@ get_header(); ?>
 
 			<div class="section1 col-xs-8 col-md-8 col-xs-offset-2 row">
 
-				<div class="owner-box col-xs-6 col-md-6">
+				<div class="owner-box owner1 col-xs-6 col-md-6">
 					<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
 					<h4>Łukasz Kuźmiński</h4>
 					<p>co-owner</p>
@@ -233,5 +234,53 @@ get_header(); ?>
 		</div>
 
 	</div>
+
+
+
+
+
+
+	<div class="owner1-details hidden container">
+		
+		<div class="popupbox col-xs-10 col-md-10 col-xs-offset-1 col-md-offset-1">
+
+			<div class="owner-box owner1 col-xs-4 col-md-4">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="placeholder" class="img-responsive img-fluid ">
+			</div>
+
+			<div class="owner-box owner1 col-xs-8 col-md-8">
+				<h3>Łukasz Kuźmiński</h3>
+				<p class="role">co-owner</p>
+				<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus porro, enim earum harum eum neque, placeat quam praesentium expedita nulla dolor odio perferendis doloribus similique blanditiis suscipit. Ipsum et ad maiores sapiente vitae hic natus expedita, dignissimos voluptatibus libero perspiciatis, vero adipisci! Voluptas quae ipsa quidem quia sint quibusdam ratione doloribus ut, provident tenetur nemo maiores, ea minus molestiae eligendi nam beatae qui dolores, nihil! Nam voluptates illo ipsam labore in quia nisi dolores sed unde impedit? Hic quaerat provident sed voluptatum tempore doloremque. In blanditiis, eaque molestiae eum numquam voluptatem dolores exercitationem alias repellat! Earum, quasi dignissimos reiciendis ipsum.
+				</p>
+				<p class="contact-info">
+					+ 48 123 456789
+				</p>
+				<p class="contact-info">
+					rafal@skiactive.com
+				</p>
+			</div>
+			<img src="<?php echo get_template_directory_uri(); ?>/img/x_button.png" class="img-responsive img-fluid x_button exit">
+		</div>
+		
+
+	</div>
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
 <?php get_footer(); ?>
--- js/app.js
+++ js/app.js
@@ -2,4 +2,49 @@ $(document).ready(function(){
   $('.contact-img').click(function() {
     $('.contact-info').toggleClass("hidden");
   });
+
+function showForm() {
+		// Change button color
+
+		// Hide scrollbar
+		$('body').addClass("disable-scroll");
+		// Show black overlay
+		showAnimation($('.overlay'), 0.5);
+		// Show form
+		showAnimation($('.detal'), 1);
+	}
+
+	function showAnimation(selectedObject, opacity) {
+		selectedObject.removeClass('hidden');
+		selectedObject.animate({opacity: opacity}, 300);
+	}
+
+	function hideAnimation(selectedObject) {
+		selectedObject.animate({opacity: 0}, 300, function(){selectedObject.addClass('hidden')});
+	}
+
+	function close() {
+		hideAnimation($('.overlay'));
+		hideAnimation($('.owner1-details'));
+		$('body').removeClass("disable-scroll");
+		// Remove red validation text and border when form is closed
+	};
+
+	$('.owner1').click(function(){
+		$('body').addClass("disable-scroll");
+		showAnimation($('.overlay'), 0.5);
+		showAnimation($('.owner1-details'), 1);
+	});
+
+
+	$('.overlay').click(function(){
+		close();
+	});
+
+	$('.exit').click(function(){
+		close();
+	});
+
 });
+
+
