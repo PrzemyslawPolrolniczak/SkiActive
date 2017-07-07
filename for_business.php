716d6619298a7df291bd96fa8d9ac19ca483e1cd
Przemyslaw Polrolniczak <przemekp96@gmail.com>
2017-07-07 12:28:08 +0200

    added JS modal functions

--- for_business.php
+++ for_business.php
@@ -15,6 +15,7 @@
  */
 get_header(); ?>
 <div class="for_business-site">
+  <div class="overlay hidden"></div>
   <div class="container">
     <div class="row section-one">
      <div class="col-sm-6 first-text-section">
@@ -26,13 +27,13 @@ get_header(); ?>
      </div>
      <div class="col-sm-5 col-xs-offset-1 first-photo-section">
        <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid">
-       <button type="button" name="button" class="btn btn-primary btn-outline">Sprawdź atrakcje zimowe</button>
+       <button type="button" name="button" class="btn btn-primary btn-outline winter">Sprawdź atrakcje zimowe</button>
      </div>
    </div>
    <div class="row seciton-two">
      <div class="col-sm-5 second-photo-section">
        <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid">
-       <button type="button" name="button" class="btn btn-primary btn-outline">Sprawdź atrakcje letnie</button>
+       <button type="button" name="button" class="btn btn-primary btn-outline summer">Sprawdź atrakcje letnie</button>
      </div>
      <div class="col-sm-6 col-xs-offset-1 second-text-section">
        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
@@ -48,7 +49,7 @@ get_header(); ?>
      </div>
      <div class="col-xs-5 col-xs-offset-1 third-photo-section">
       <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid">
-      <button type="button" name="button" class="btn btn-primary btn-outline">Sprawdź MICE ACTIVE</button>
+      <button type="button" name="button" class="btn btn-primary btn-outline mice-active">Sprawdź MICE ACTIVE</button>
      </div>
    </div>
    <div class="text-at-the-end">
@@ -58,5 +59,66 @@ get_header(); ?>
  <div class="question-stripe">
    <h1>Masz pytania dotyczące oferty dla biznesu?</h1> <img src="<?php echo get_template_directory_uri(); ?>/img/arrows.png" alt="" class="img-responsive img-fluid">
  </div>
+
+
+  <div class="detal hidden winter-detal">
+    <div class="container header">
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-1">ATRAKCJE ZIMOWE</div>
+        <div class="col-xs-1 col-xs-offset-7 exit">X</div>
+      </div>
+    </div>
+    <div class="container content">
+      <div class='row'>
+       <div class='col-md-8'>
+         <div class="carousel slide media-carousel" id="media">
+           <div class="carousel-inner">
+             <div class="item  active">
+               <div class="row">
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+               </div>
+             </div>
+             <div class="item">
+               <div class="row">
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+               </div>
+             </div>
+             <div class="item">
+               <div class="row">
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+                 <div class="col-md-4">
+                   <a class="thumbnail" href="#"><img alt="" src="http://placehold.it/150x150"></a>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <a data-slide="prev" href="#media" class="left carousel-control">‹</a>
+           <a data-slide="next" href="#media" class="right carousel-control">›</a>
+         </div>
+       </div>
+     </div>
+    </div>
+  </div>
 </div>
 <?php get_footer(); ?>
--- js/app.js
+++ js/app.js
@@ -2,4 +2,45 @@ $(document).ready(function(){
   $('.contact-img').click(function() {
     $('.contact-info').toggleClass("hidden");
   });
+
+  function showForm() {
+		// Change button color
+
+		// Hide scrollbar
+		$('body').addClass("disable-scroll");
+		// Show black overlay
+		showAnimation($('.overlay'), 0.5);
+		// Show form
+		showAnimation($('.detal'), 1);
+	}
+
+	function showAnimation(selectedObject, opacity) {
+		selectedObject.removeClass('hidden');
+		selectedObject.animate({opacity: opacity}, 300);
+	}
+
+	function hideAnimation(selectedObject) {
+		selectedObject.animate({opacity: 0}, 300, function(){selectedObject.addClass('hidden')});
+	}
+
+	function close() {
+		hideAnimation($('.overlay'));
+		hideAnimation($('.detal'));
+		$('body').removeClass("disable-scroll");
+		// Remove red validation text and border when form is closed
+	};
+
+	$('.winter').click(function(){
+		$('body').addClass("disable-scroll");
+		showAnimation($('.overlay'), 0.5);
+		showAnimation($('.detal'), 1);
+	});
+
+	$('.overlay').click(function(){
+		close();
+	});
+
+	$('.exit').click(function(){
+		close();
+	});
 });
