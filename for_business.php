ef3a6d9ba6077084b35d7d2a8376b80a16dd3289
Przemyslaw Polrolniczak <przemekp96@gmail.com>
2017-07-11 18:21:32 +0200

    working carousels

--- for_business.php
+++ for_business.php
@@ -26,13 +26,13 @@ get_header(); ?>
        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</p>
      </div>
      <div class="col-sm-5 col-xs-offset-1 first-photo-section">
-       <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid">
+       <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid shadowed">
        <button type="button" name="button" class="btn btn-primary btn-outline winter">Sprawdź atrakcje zimowe</button>
      </div>
    </div>
    <div class="row seciton-two">
      <div class="col-sm-5 second-photo-section">
-       <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid">
+       <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid shadowed">
        <button type="button" name="button" class="btn btn-primary btn-outline summer">Sprawdź atrakcje letnie</button>
      </div>
      <div class="col-sm-6 col-xs-offset-1 second-text-section">
@@ -48,7 +48,7 @@ get_header(); ?>
        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud e xercitationt xercitationt xercitationt</p>
      </div>
      <div class="col-xs-5 col-xs-offset-1 third-photo-section">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/photo1.png" alt="" class="img-responsive img-fluid shadowed">
       <button type="button" name="button" class="btn btn-primary btn-outline mice-active">Sprawdź MICE ACTIVE</button>
      </div>
    </div>
@@ -56,28 +56,164 @@ get_header(); ?>
      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, doloremque fugiat in ducimus saepe numquam, molestiae reiciendis voluptates illum itaque quae eum quidem debitis expedita</p>
    </div>
  </div>
- <div class="question-stripe">
-   <h1>Masz pytania dotyczące oferty dla biznesu?</h1> <img src="<?php echo get_template_directory_uri(); ?>/img/arrows.png" alt="" class="img-responsive img-fluid">
+ <div class="question-stripe slider">
+   <div>
+     <h1>Masz pytania dotyczące oferty dla biznesu?</h1> <img src="<?php echo get_template_directory_uri(); ?>/img/arrows.png" alt="" class="img-responsive img-fluid">
+   </div>
+   <div>
+     <h1>Tel: +48 123 456 789 | E-mail: test@gmail.com</h1>
+   </div>
  </div>
 
 
-  <div class="detal hidden winter-detal">
+  <div class="detal hidden component-detal winter-detal">
     <div class="container header">
       <div class="row">
         <div class="col-xs-2 col-xs-offset-1">ATRAKCJE ZIMOWE</div>
-        <div class="col-xs-1 col-xs-offset-7 exit">X</div>
+        <div class="col-xs-1 col-xs-offset-7 exit"><img src="<?php echo get_template_directory_uri(); ?>/img/exitButton.png" alt="" class="img-responsive img-fluid"></div>
       </div>
     </div>
     <div class="container content">
       <div class='row'>
-         <div class="slider">
-          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide1.png" alt="" class="img-responsive img-fluid slide-image"></div>
-          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide2.png" alt="" class="img-responsive img-fluid slide-image"></div>
-          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide3.png" alt="" class="img-responsive img-fluid slide-image"></div>
-          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide4.png" alt="" class="img-responsive img-fluid slide-image"></div>
-          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide5.png" alt="" class="img-responsive img-fluid slide-image"></div>
+         <div class="slider sliderWinter">
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide1.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide2.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide3.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide4.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide5.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
         </div>
      </div>
+     <div class="row text-row">
+       <div class="col-xs-offset-2 col-xs-8 text-col">
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+       </div>
+     </div>
+    </div>
+    <div class="container footer">
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-1 leftText mice-active-link"><img src="<?php echo get_template_directory_uri(); ?>/img/leftButton.png" alt="" class="img-responsive img-fluid arrow left-arrow">MICE ACTIVE</div>
+        <div class="col-xs-2 col-xs-offset-6 rightText summer-link">ATRAKCJE LETNIE<img src="<?php echo get_template_directory_uri(); ?>/img/rightButton.png" alt="" class="img-responsive img-fluid arrow right-arrow"></div>
+      </div>
+    </div>
+  </div>
+  <div class="detal hidden component-detal summer-detal">
+    <div class="container header">
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-1">ATRAKCJE LETNIE</div>
+        <div class="col-xs-1 col-xs-offset-7 exit"><img src="<?php echo get_template_directory_uri(); ?>/img/exitButton.png" alt="" class="img-responsive img-fluid"></div>
+      </div>
+    </div>
+    <div class="container content">
+      <div class='row'>
+         <div class="slider sliderSummer">
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide1.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide2.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide3.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide4.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide5.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+        </div>
+     </div>
+     <div class="row text-row">
+       <div class="col-xs-offset-2 col-xs-8 text-col">
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+       </div>
+     </div>
+    </div>
+    <div class="container footer">
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-1 leftText winter-link"><img src="<?php echo get_template_directory_uri(); ?>/img/leftButton.png" alt="" class="img-responsive img-fluid arrow left-arrow">ATRAKCJE ZIMOWE</div>
+        <div class="col-xs-2 col-xs-offset-6 rightText mice-active-link">MICE ACTIVE<img src="<?php echo get_template_directory_uri(); ?>/img/rightButton.png" alt="" class="img-responsive img-fluid arrow right-arrow"></div>
+      </div>
+    </div>
+  </div>
+  <div class="detal hidden component-detal mice-active-detal">
+    <div class="container header">
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-1">MICE ACTIVE</div>
+        <div class="col-xs-1 col-xs-offset-7 exit"><img src="<?php echo get_template_directory_uri(); ?>/img/exitButton.png" alt="" class="img-responsive img-fluid"></div>
+      </div>
+    </div>
+    <div class="container content">
+      <div class='row'>
+         <div class="slider sliderMiceActive">
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide1.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide2.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide3.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide4.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+          <div><img src="<?php echo get_template_directory_uri(); ?>/img/slide5.png" alt="" class="img-responsive img-fluid slide-image shadowed"></div>
+        </div>
+     </div>
+     <div class="row text-row">
+       <div class="col-xs-offset-2 col-xs-8 text-col">
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+         <div class="col-xs-4">
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+           <p><div class="hyphen-before"></div> Lorem ipsum dolor</p>
+         </div>
+       </div>
+     </div>
+    </div>
+    <div class="container footer">
+      <div class="row">
+        <div class="col-xs-2 col-xs-offset-1 leftText summer-link"><img src="<?php echo get_template_directory_uri(); ?>/img/leftButton.png" alt="" class="img-responsive img-fluid arrow left-arrow">ATRAKCJE LETNIE</div>
+        <div class="col-xs-2 col-xs-offset-6 rightText winter-link">ATRAKCJE ZIMOWE<img src="<?php echo get_template_directory_uri(); ?>/img/rightButton.png" alt="" class="img-responsive img-fluid arrow right-arrow"></div>
+      </div>
     </div>
   </div>
 </div>
--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 		hideAnimation($('.overlay'));
 		hideAnimation($('.owner1-details'));
 		hideAnimation($('.owner2-details'));
-		hideAnimation($('.detal'));
+		$('.detal').addClass('hidden');
     hideAnimation($('.regulations-details'));
 		$('body').removeClass("disable-scroll");
 	};
@@ -43,19 +43,143 @@ $(document).ready(function(){
 		showAnimation($('.owner2-details'), 1);
 	});
 
+ //UNSLCIK FUNCTION
+  function unslickSlick(slick1, slick2, slick3) {
+
+    if ($(slick1).hasClass('slick-initialized') == true) {
+      $(slick1).slick('unslick');
+    }
+    if ($(slick2).hasClass('slick-initialized') == true) {
+      $(slick2).slick('unslick');
+    }
+    if ($(slick3).hasClass('slick-initialized') == true) {
+      $(slick3).slick('unslick');
+    }
+  }
+
+//SLICK FUNCTION
+
+
+$('.question-stripe').slick({
+  dots: false,
+  slidesToShow: 1,
+  infinite: false,
+  speed: 300,
+  accessibility: false,
+  arrows: false,
+  draggable: false,
+  nextArrow: $('.question-stripe'),
+});
+
+$('.question-stripe').click(function () {
+  $(this).slick('slickNext');
+});
+
+function initSlider(sliderName) {
+    $(sliderName).slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      dots: true,
+      draggable: false,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  }
+
+//HIDE FUNCTION
+  function hideModal(first, second) {
+    $(first).addClass('hidden');
+    $(first).css('opacity', "0");
+    $(second).addClass('hidden');
+    $(second).css('opacity', "0");
+  }
+//////////////
+
 	$('.winter').click(function(){
 		$('body').addClass("disable-scroll");
 		showAnimation($('.overlay'), 0.5);
-		showAnimation($('.detal'), 1);
+		showAnimation($('.winter-detal'), 1);
+    unslickSlick('.sliderSummer', '.sliderMiceActive');
+    if ($('.sliderWinter').hasClass('slick-initialized') == false) {
+      initSlider('.sliderWinter');
+    }
+	});
+
+  $('.summer').click(function(){
+		$('body').addClass("disable-scroll");
+		showAnimation($('.overlay'), 0.5);
+		showAnimation($('.summer-detal'), 1);
+    unslickSlick('.sliderWinter', '.sliderMiceActive');
+    if ($('.sliderSummer').hasClass('slick-initialized') == false) {
+      initSlider('.sliderSummer');
+    }
+	});
+
+  $('.mice-active').click(function(){
+		$('body').addClass("disable-scroll");
+		showAnimation($('.overlay'), 0.5);
+		showAnimation($('.mice-active-detal'), 1);
+    unslickSlick('.sliderWinter', '.sliderSummer');
+    if ($('.sliderMiceActive').hasClass('slick-initialized') == false) {
+      initSlider('.sliderMiceActive');
+    }
 	});
 
 	$('.overlay').click(function(){
 		close();
+    unslickSlick('.sliderSummer', '.sliderWinter', '.sliderMiceActive');
 	});
 
 	$('.exit').click(function(){
 		close();
+    unslickSlick('.sliderSummer', '.sliderWinter', '.sliderMiceActive');
 	});
+
+  $('.mice-active-link').click(function () {
+    hideModal('.winter-detal', '.summer-detal');
+    showAnimation($('.mice-active-detal'), 1);
+    unslickSlick('.sliderWinter', '.sliderSummer');
+    if ($('.sliderMiceActive').hasClass('slick-initialized') == false) {
+      initSlider('.sliderMiceActive');
+    }
+  });
+
+  $('.winter-link').click(function () {
+    hideModal('.mice-active-detal', '.summer-detal');
+    showAnimation($('.winter-detal'), 1);
+    unslickSlick('.sliderSummer', '.sliderMiceActive');
+    if ($('.sliderWinter').hasClass('slick-initialized') == false) {
+      initSlider('.sliderWinter');
+    }
+  });
+
+  $('.summer-link').click(function () {
+    hideModal('.winter-detal', '.mice-active-detal');
+    showAnimation($('.summer-detal'), 1);
+    unslickSlick('.sliderWinter', '.sliderMiceActive');
+    if ($('.sliderSummer').hasClass('slick-initialized') == false) {
+      initSlider('.sliderSummer');
+    }
+  });
 
   function showFormRegulaminy() {
 		// Hide scrollbar
@@ -66,38 +190,9 @@ $(document).ready(function(){
 		showAnimation($('.regulations-details'), 1);
 	}
 
-	$('.regulations').click(function(){
+	$('.regulations').click(function() {
 		$('body').addClass("disable-scroll");
 		showAnimation($('.overlay'), 0.5);
 		showAnimation($('.regulations-details'), 1);
 	});
-
-  $('.slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    dots: true,
-    draggable: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-
 });
